26c19a3f47cd45a5fedd6cda44be29bc54da6041
RakhebLI <r.litvinenko@leadintelligence.biz>
2019-06-04 14:51:29 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // js code
-document.getElementById("js-btn").addEventListener("click", () => {
-  alert("JS compiles!!");
+document.getElementById('js-btn').addEventListener('click', () => {
+	alert('JS compiles!!');
 });
 
 // jquery code
 $(document).ready(function() {
-  $("#jquery-btn").click(function() {
-    alert("jQuery Works.");
-  });
+	$('#jquery-btn').click(function() {
+		alert('jQuery Works.');
+	});
 });
